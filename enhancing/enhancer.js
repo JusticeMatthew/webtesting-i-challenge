fe42e0d14a0fa60339cc98630ad66dc8a4005808
Matthew Justice <matthewajustice@gmail.com>
2021-02-26 00:33:50 -0500

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,21 +2,29 @@ module.exports = {
   success,
   fail,
   repair,
-  get,
 };
 
 function success(item) {
-  return { ...item };
+  if (item.enhancement === 20) {
+    return item;
+  } else {
+    item.enhancement++;
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    item.durability -= 5;
+  } else {
+    item.durability -= 10;
+  }
+
+  if (item.enhancement >= 16) {
+    item.enhancement--;
+  }
 }
 
 function repair(item) {
-  return { ...item };
-}
-
-function get(item) {
-  return { ...item };
+  item.durability = 100;
+  return item;
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,56 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+let mjolnir = {};
+
+beforeEach(() => {
+  (mjolnir.name = 'Mjolnir'),
+    (mjolnir.durability = 90),
+    (mjolnir.enhancement = 14);
+});
+describe('Existance', () => {
+  it('Am I sane?', () => {
+    expect(1).toBe(1);
+  });
+});
+
+describe('Repair Items', () => {
+  it('Restore item durability', () => {
+    enhancer.repair(mjolnir);
+    expect(mjolnir.durability).toBe(100);
+  });
+
+  describe('Successfuly Enhanced Items', () => {
+    it('Item enhancement level increases', () => {
+      expect(mjolnir.enhancement).toBe(14);
+      enhancer.success(mjolnir);
+      expect(mjolnir.enhancement).toBe(15);
+      expect(mjolnir.durability).toBe(90);
+    });
+  });
+
+  describe('Failed Enhanced Items', () => {
+    it('Item enhancement < 15, -5 durability', () => {
+      expect(mjolnir.enhancement).toBe(14);
+      expect(mjolnir.durability).toBe(90);
+      enhancer.fail(mjolnir);
+      expect(mjolnir.enhancement).toBe(14);
+      expect(mjolnir.durability).toBe(85);
+    });
+    it('Item enhancement > 15, -10 durability', () => {
+      mjolnir.enhancement = 15;
+      expect(mjolnir.enhancement).toBe(15);
+      expect(mjolnir.durability).toBe(90);
+      enhancer.fail(mjolnir);
+      expect(mjolnir.enhancement).toBe(15);
+      expect(mjolnir.durability).toBe(80);
+    });
+    it('Item enhancement > 16, -10 durability, -1 enhancement', () => {
+      mjolnir.enhancement = 17;
+      expect(mjolnir.enhancement).toBe(17);
+      expect(mjolnir.durability).toBe(90);
+      enhancer.fail(mjolnir);
+      expect(mjolnir.enhancement).toBe(16);
+      expect(mjolnir.durability).toBe(80);
+    });
+  });
+});
